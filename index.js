9b149f89c4c85cded04754a696de3c2aaf963bc2
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-04-02 16:44:16 -0400

    array.find

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(array){
+    const isWin = array.find(element => element.result ==="W")
+    if (isWin !== undefined){
+    return array.find(element => element.result ==="W").year
+    }
+}
